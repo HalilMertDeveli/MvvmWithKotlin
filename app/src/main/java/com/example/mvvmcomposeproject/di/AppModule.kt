8ffb0de21e3bb76effa3b632f203da7598a121a2
haliko <halilmertdeveliii@gmail.com>
2023-07-13 14:44:16 +0300

    i finished the digger hill sthings

---
+++ app/src/main/java/com/example/mvvmcomposeproject/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.example.mvvmcomposeproject.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.mvvmcomposeproject.data.TodoDatabase
+import com.example.mvvmcomposeproject.data.TodoRepository
+import com.example.mvvmcomposeproject.data.TodoRepositoryImp
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app:Application):TodoDatabase{
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db:TodoDatabase):TodoRepository{
+        return TodoRepositoryImp(db.dao)
+    }
+}
