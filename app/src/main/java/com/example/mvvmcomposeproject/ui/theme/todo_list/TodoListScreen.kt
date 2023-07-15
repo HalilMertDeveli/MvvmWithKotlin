ae78dbbc985ba0faef756860cd6a540e970fc538
haliko <halilmertdeveliii@gmail.com>
2023-07-16 00:11:32 +0300

    i will design TodoListScreen

---
+++ app/src/main/java/com/example/mvvmcomposeproject/ui/theme/todo_list/TodoListScreen.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmcomposeproject.ui.theme.todo_list
+
+import androidx.compose.runtime.Composable
+import com.example.mvvmcomposeproject.util.UiEvent
+
+@Composable
+fun TodoListScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+
+    ) {
+
+}
